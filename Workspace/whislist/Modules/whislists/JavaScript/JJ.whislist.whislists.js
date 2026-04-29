70d491a88322791281faa3ce5961f79cde1b75af
Alfi-Francis-k <alfi.francis@jobinandjismi.com>
2026-04-29 10:15:24 +0530

    whislist

--- Workspace/whislist/Modules/whislists/JavaScript/JJ.whislist.whislists.js
+++ Workspace/whislist/Modules/whislists/JavaScript/JJ.whislist.whislists.js
@@ -1,38 +1,3 @@
-
-// define(
-// 	'JJ.whislist.whislists'
-// ,   [
-// 		'JJ.whislist.whislists.View'
-// 	]
-// ,   function (
-// 		whislistsView
-// 	)
-// {
-// 	'use strict';
-
-// 	return  {
-// 		mountToApp: function mountToApp (container)
-// 		{
-// 			// using the 'Layout' component we add a new child view inside the 'Header' existing view 
-// 			// (there will be a DOM element with the HTML attribute data-view="Header.Logo")
-// 			// more documentation of the Extensibility API in
-// 			// https://system.netsuite.com/help/helpcenter/en_US/APIs/SuiteCommerce/Extensibility/Frontend/index.html
-			
-// 			/** @type {LayoutComponent} */
-// 			var layout = container.getComponent('Layout');
-			
-// 			if(layout)
-// 			{
-// 				layout.addChildView('Header.Logo', function() { 
-// 					return new whislistsView({ container: container });
-// 				});
-// 			}
-
-// 		}
-// 	};
-// });
-
-
 define('JJ.whislist.whislists', [
     'Facets.ItemCell.View',
     'JJ.whislist.whislists.View',
